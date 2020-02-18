18065baa67b7e47c455d6e44b4b61a827b02b40d
rawan <rawan.designer@hotmail.com>
2020-02-18 15:52:40 +0200

    Adding the App structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
+// app.use(express.static(__dirname + '../Public'));
+
+app.use(express.static(path.join(__dirname, '..','Public')));
+
 app.get("/", function(req, res) {
   res.send("");
-
- 
 });
+
 
 module.exports = app;
